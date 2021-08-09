2d87d867f2c1f45fdaba6d7a1d242f7288136e1d
Jeferson Cruz <dev.blackfishlabs@gmail.com>
2021-08-08 21:09:57 -0400

    fix strategy

--- strategy.py
+++ strategy.py
@@ -5,24 +5,25 @@ def main():
     """main function"""
     api = "http://159.65.193.229:9090/fetch/greenblatt"
     try:
-        data = requests.get(api, timeout=30).json()
+        data = requests.get(api).json()
         for x in data["data"]:
-            api = "http://159.65.193.229:9090/fetch/indicators?stock=" + \
-                x["ticker"] + ".SA"
-            try:
-                response = requests.get(api, timeout=30).json()
-                if float(response["data"]["rsi14"]) < 30:
-                    print("===============================")
-                    print("COMPANY  >> " + str(x["companyName"]))
-                    print("SCORE    >> " + str(x["final_Score"]))
-                    print("BUY      >> " + response["data"]["stock"])
-                    print("RSI      >> " + response["data"]["rsi14"])
-                    print("EMA      >> " + response["data"]["ema56"])
+            if x["final_Score"] > 200:
+                api = "http://159.65.193.229:9090/fetch/indicators?stock=" + \
+                    x["ticker"] + ".SA"
+                try:
+                    response = requests.get(api).json()
+                    if float(response["data"]["rsi14"]) <= 30:
+                        print("===============================")
+                        print("COMPANY  >> " + str(x["companyName"]))
+                        print("SCORE    >> " + str(x["final_Score"]))
+                        print("BUY      >> " + response["data"]["stock"])
+                        print("RSI      >> " + response["data"]["rsi14"])
+                        print("EMA      >> " + response["data"]["ema56"])
 
-            except requests.exceptions.RequestException:
-                return False
-            except (ValueError):
-                return False
+                except requests.exceptions.RequestException:
+                    return False
+                except (ValueError):
+                    return False
     except requests.exceptions.RequestException:
         return False
     except (ValueError):
